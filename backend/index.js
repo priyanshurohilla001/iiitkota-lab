3e345dd9ded3f1ad677a15e451552a33389f13c1
Priyanshu Rohilla <priyanshurohilla001@gmail.com>
2024-05-14 01:50:58 +0530

    welcome api message

--- backend/index.js
+++ backend/index.js
@@ -1,17 +1,16 @@
-import express from 'express';
-import cors from 'cors';
-import mainRouter from './routes/index.js';
+import express from "express";
+import cors from "cors";
+import mainRouter from "./routes/index.js";
 
 const app = express();
 
-
 app.use(cors());
 app.use(express.json());
-app.use('/api/v1', mainRouter);
-
-app.listen(3000, () => {
-    console.log("Server started at port 3000");
+app.use("/api/v1", mainRouter);
+app.use("/", (req, res) => {
+  res.send("Welcome to the IIIT API");
 });
 
-
-
+app.listen(3000, () => {
+  console.log("Server started at port 3000");
+});
--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,10 +1,11 @@
-import express from 'express';
-import emailVerificationRouter from './emailverification.js';
-import userRouter from './user.js';
+import express from "express";
+import emailVerificationRouter from "./emailverification.js";
+import userRouter from "./user.js";
 
 const router = express.Router();
 
-router.use('/emailverification', emailVerificationRouter);
-router.use('/user', userRouter);
+router.use("/emailverification", emailVerificationRouter);
+router.use("/user", userRouter);
+router.use("/", (req, res) => res.send("Hello from iiitk"));
 
 export default router;
